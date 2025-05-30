53496014a3aec06d11ded82f967e8b55a9381dcf
Jade Figueredo <jadefigueredo@outlook.com>
2025-05-30 14:12:17 -0300

    fix: adiciona endpoint para health

--- api/app.py
+++ api/app.py
@@ -12,6 +12,10 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify(status="ok", message="API is running"), 200
+
 @app.route("/api/clothing", methods=["POST"])
 def postRequest():
     try:
